3ff3dd1838fcaf274c6e51d14a1e33f3fcd08d39
David Feng <feng.xingxu@gmail.com>
2020-11-25 18:32:54 -0600

    numbers

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+def numbers():
+    for n in range(1,10):
+        print(n)
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -12,7 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    numbers()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 print("bye")
